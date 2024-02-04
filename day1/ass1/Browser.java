039043677c9140e5cd9e4dccbfa3ee0253b33f66
Vidhya-Victory89 <158831825+Vidhya-Victory89@users.noreply.github.com>
2024-02-04 22:26:11 +0530

    Add files via upload
    Vidhya Home Assignment1

---
+++ day1/ass1/Browser.java
@@ -0,0 +1,20 @@
+package day1.ass1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		float browserVersion= 100.2f;
+		String browserName="FireFox";
+		boolean pageLoaded=true;
+		int noOfBrowser=1;
+		char browserLogo='f';
+		System.out.println("\tDemonstrating Variables in Java class\n");
+		System.out.println("\tBrowser Version:" +browserVersion);
+		System.out.println("\tBrowser Name:" +browserName);
+		System.out.println("\tBrowser Page Loaded:" +pageLoaded);
+		System.out.println("\tBrowser Number of Browser:" +noOfBrowser);
+		System.out.println("\tBrowser Logo:" +browserLogo);
+
+	}
+
+}
---
+++ day1/ass1/FindNumber.java
@@ -0,0 +1,19 @@
+package day1.ass1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int a=45;
+		System.out.println("\tDemonstrating If-Else Condition\n");
+		if(a>0)
+		{
+			System.out.println("\tThe number is positive");
+		}
+		else
+		{
+			System.out.println("\tThe number is negative");
+		}
+
+	}
+
+}
